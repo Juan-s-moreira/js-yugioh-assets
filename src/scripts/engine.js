7d72255a7c7fe346784cfcb92f62abdf51c0e721
Juan Santos <juan.moreira62@outlook.com>
2024-10-22 21:16:35 -0300

    criandoa enumeração de cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -20,6 +20,34 @@ const state = {
     },
 }
 
+const patjImages = ".src/assets/icons/";
+
+const cardData = [
+    {
+        id:0,
+        name:"Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${patjImages}dragon.png`,
+        winof:[1],
+        loseOf: [2],
+    },
+    {
+        id:1,
+        name:"Dark Magician",
+        type: "Rock",
+        img: `${patjImages}magician.png`,
+        winof:[2],
+        loseOf: [0],
+    },
+    {
+        id:2,
+        name:"Exodia",
+        type: "Scissor",
+        img: `${patjImages}exodia.png`,
+        winof:[0],
+        loseOf: [1],
+    },
+]
 
 
 function init(){}
