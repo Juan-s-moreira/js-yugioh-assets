43604f34e9b631241bbffbc85a46c748f0577cf5
Juan Santos <juan.moreira62@outlook.com>
2024-10-22 22:13:11 -0300

    fixing bugs

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -21,11 +21,11 @@ const state = {
 }
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
 
-const patjImages = ".src/assets/icons/";
+const patjImages = "./src/assets/icons/";
 
 const cardData = [
     {
